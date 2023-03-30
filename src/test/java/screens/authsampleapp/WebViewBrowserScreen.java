da4ef6939a951a2e9e806cba099166b55adff6dd
neiger <neiger.serrano.wks@gmail.com>
2023-03-30 12:35:37 -0600

    update package name and. main activity

--- src/test/java/screens/authsampleapp/WebViewBrowserScreen.java
+++ src/test/java/screens/authsampleapp/WebViewBrowserScreen.java
@@ -5,7 +5,6 @@ import general.ErrorsManager;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
